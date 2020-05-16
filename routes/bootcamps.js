bd5bbe2cc2d50b66350cfefd3701b261baf8d371
Ricardo Gomes <rjdsgomes@gmail.com>
2020-05-16 18:47:06 +0100

    Devcamper project

---
+++ routes/bootcamps.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload,
+} = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
+// Include other resource routers
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
+const router = express.Router();
+
+const { protect, authorize } = require('../middleware/auth');
+//
+
+// Re-route into other resourced routers
+router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,93 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
+const morgan = require('morgan');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+const colors = require('colors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect DB
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// File upload
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate Limit
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10mins
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server rungging in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server rungging in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
 );
+
+// Handle unhandle rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server
+  server.close(() => process.exit(1));
+});
